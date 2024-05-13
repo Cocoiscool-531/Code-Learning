7eb4951b56da2b4a65662127bb9ea0882fa9d60d
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2024-05-13 18:49:19 +0000

    we

--- C++/Snake/output.cpp
+++ C++/Snake/output.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <unistd.h>
 
-int main(){
-    
-}
+int main(int argc, char *argv[]){
+  textbackground(BLUE);
+  clrscr();
+  textcolor(WHITE);
+  gotoxy(30,5);
+  cprintf("Hello World WHITE");
+  textcolor(YELLOW);
+  gotoxy(30,6);
+  cprintf("Hello World YELLOW");
+  for(int i=0;i<16;i++){
+    textcolor(i);
+    gotoxy(1,2+i);
+    cprintf("Hello World");
+    }
+  getch();
+  return 0;
+  }
