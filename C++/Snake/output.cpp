42fa4f2b7bade0077cfee4b9c9af4d8dffd6dfd3
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2024-05-14 13:14:41 +0000

    seting up pages

--- C++/Snake/output.cpp
+++ C++/Snake/output.cpp
@@ -1,20 +1,19 @@
-#include <iostream>
-#include <unistd.h>
+#include <conio.h>
 
 int main(int argc, char *argv[]){
-  textbackground(BLUE);
-  clrscr();
+  /*setBackgroundColor(BLUE);
+  clearscreen();
   textcolor(WHITE);
   gotoxy(30,5);
   cprintf("Hello World WHITE");
-  textcolor(YELLOW);
-  gotoxy(30,6);
+  setTextColor(YELLOW);
+  gotoxy(30,6)
   cprintf("Hello World YELLOW");
   for(int i=0;i<16;i++){
     textcolor(i);
     gotoxy(1,2+i);
     cprintf("Hello World");
     }
-  getch();
-  return 0;
+  getc();
+  return 0;*/
   }
